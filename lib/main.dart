3ea2ec202c753f2c39c28b5c98fffdfd5211f115
Decryptic <gageswenson@gmail.com>
2023-06-18 21:47:52 -1000

    todo: implement action kind logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
 
 // Gage Swenson @Decryptic
@@ -43,15 +41,57 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final _icon_size = 42.0;
+
+  late final Record _audioRecorder;
   bool _recording = false;
+  int _duration = 0;
+  Timer? _timer;
 
   @override
   void initState() {
+    _audioRecorder = Record();
+
     super.initState();
+  }
+
+  Future<void> _start() async {
+    try {
+      if (await _audioRecorder.hasPermission()) {
+        const encoder = AudioEncoder.aacLc;
+        final isSupported = await _audioRecorder.isEncoderSupported(encoder);
+        debugPrint('${encoder.name} supported: $isSupported');
+
+        await _audioRecorder.start();
+        _duration = 0;
+        _startTimer();
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> _stop() async {
+    _timer?.cancel();
+    _duration = 0;
+    final path = await _audioRecorder.stop();
+    if (path == null) {
+      debugPrint('path is null on stop');
+    }
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() => _duration++);
+    });
   }
 
   @override
   void dispose() {
+    _timer?.cancel();
+    _audioRecorder.dispose();
     super.dispose();
   }
 
@@ -66,13 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '00:00',
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
+            _buildTimer(),
             const Image(
               image: const AssetImage(
                 'assets/images/tape.png',
@@ -116,7 +150,29 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         },
         tooltip: _recording ? 'Stop' : 'Record',
-        child: _recording ? const Icon(Icons.stop) : const Icon(Icons.fiber_manual_record),
+        child: _recording 
+          ? const Icon(Icons.stop) 
+          : const Icon(Icons.fiber_manual_record, color: Colors.red),
+      ),
+    );
+  }
+
+  Widget _buildTimer() {
+    final String Function(int) _formatNumber = (sec) {
+      String numberStr = sec.toString();
+      if (sec < 10) {
+        numberStr = '0$numberStr';
+      }
+      return numberStr;
+    };
+
+    final String minutes = _formatNumber(_duration ~/ 60);
+    final String seconds = _formatNumber(_duration % 60);
+    return Text(
+      '$minutes : $seconds',
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 30,
       ),
     );
   }
